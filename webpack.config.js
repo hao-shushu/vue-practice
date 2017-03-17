d22fba1df66759c41fe6611287d7c28fe72e654a
soloproc <liejin.huang@gmail.com>
2017-03-17 09:37:36 +0800

    simple router

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+	entry: './src/main.js',
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		publicPath: '/dist/',
+		filename: 'build.js'
+	},
+	module: {
+	    rules: [
+	        {
+	            test: /\.vue$/,
+	            use: ["vue-loader"]
+	        }
+	    ]
+	}
+};
