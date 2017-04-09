bdd65fb21643b0cbf30000261091a5e7165da4f5
haoshushu <792479611@qq.com>
2017-04-09 22:09:56 +0800

    my views

--- src/app.vue
+++ src/app.vue
@@ -3,6 +3,7 @@
         <div>
             <router-link to="/about">about</router-link>
             <router-link to="/login">login</router-link>
+            <router-link to="/my-test">my component</router-link>
         </div>
         <div>
             <router-view></router-view>
--- src/router.js
+++ src/router.js
@@ -1,12 +1,15 @@
 module.exports = {
-    routes: [
-        {
+    routes: [{
             path: '/about',
             component: require('./views/about.vue')
         },
         {
-        	path: '/login',
-        	component: require('./views/login.vue')
+            path: '/login',
+            component: require('./views/login.vue')
+        },
+        {
+            path: '/my-test',
+            component: require('./views/my-test.vue')
         }
     ]
 }
